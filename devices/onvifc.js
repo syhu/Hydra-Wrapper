b56a46509053151afe7a9e3dcc4260b32aa6825e
EastL <st100321048@gmail.com>
2015-07-12 09:15:45 +0800

    getSystemDateAndTime OK

--- devices/onvifc.js
+++ devices/onvifc.js
@@ -241,6 +241,28 @@ onvifc.prototype.getHostname = function (input) {
 		
 };
 
+onvifc.prototype.getSystemDateAndTime = function (input) {
+	var this_wrapper = this;
+	new Cam(
+		{
+			hostname: this_wrapper.data.host,
+			port: this_wrapper.data.port,
+			username: this_wrapper.data.user,
+			password: this_wrapper.data.passwd
+		},function(err){
+			this.getSystemDateandTime(function(err, stream){
+				if (stream) {
+					input.onDone(stream);
+				} else {
+					input.onFail("password error");
+				}
+			
+			});
+		}
+	);
+	
+};
+
 onvifc.prototype.setScopes = function (input) {
 	var this_wrapper = this;
 	local_obj = {
